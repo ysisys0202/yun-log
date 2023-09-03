c4cc265920c5fd4242b42b4aec0488768230f7d8
이윤슬 <ysisys0202@gmail.com>
2023-09-03 17:46:17 +0900

    TagLIst 컴포넌트 수정

--- src/components/common/TagList.tsx
+++ src/components/common/TagList.tsx
@@ -1,10 +1,29 @@
+import { css } from "@emotion/react";
 import React from "react";
 type Props = {
   align?: "start" | "center" | "end" | "between";
   children: React.ReactNode;
+  className?: string;
 };
-const TagList = ({ align = "start", children }: Props) => {
-  return <div className={`flex gap-x-1 align-${align}`}>{children}</div>;
+const TagList = ({ align = "start", children, className = "" }: Props) => {
+  return (
+    <ul css={S} className={`${className} align-${align} `}>
+      {children}
+    </ul>
+  );
 };
-
+const S = css`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 4px;
+  &.align-start {
+    justify-content: start;
+  }
+  &.align-center {
+    justify-content: center;
+  }
+  &.align-end {
+    justify-content: end;
+  }
+`;
 export default TagList;
