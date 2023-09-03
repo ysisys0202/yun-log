790f4dd28201bd889ae349264d993696f57d453e
이윤슬 <ysisys0202@gmail.com>
2023-09-03 17:34:14 +0900

    🏷️ colors 상수 추가

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  black: "#000",
+  white: "#fff",
+};
+
+export const gray = {
+  primary: " #333333",
+  secondary: "#727272",
+  border: "#e5e5e5",
+  background: "#efefef",
+};
