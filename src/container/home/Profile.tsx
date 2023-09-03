f26e5469bbc5d9d8941e48cb23f69cc6c91a5861
이윤슬 <ysisys0202@gmail.com>
2023-09-03 19:57:19 +0900

    home 페이지 Profile 섹션 추가

---
+++ src/container/home/Profile.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import HomeSection from "./HomeSection";
+import { css } from "@emotion/react";
+import SectionTitle from "@/components/home/SectionTitle";
+import { colors } from "@/constants/colors";
+const Profile = () => {
+  return (
+    <HomeSection SectionStyle={S}>
+      <SectionTitle>개발은 절거워</SectionTitle>
+      <Image
+        src="/images/home/profile.jpg"
+        alt="프로필 이미지"
+        width={430}
+        height={460}
+      />
+      <strong>프론트엔드 개발을 합니다</strong>
+      <div className="skill-set">
+        <span className="label">주요 기술</span>
+        <span className="skill-list">{skillList.join(", ")}</span>
+      </div>
+    </HomeSection>
+  );
+};
+const S = css`
+  width: 50%;
+  color: ${colors.white};
+  img {
+    margin-top: 16px;
+  }
+  strong {
+    display: flex;
+    margin-top: 8px;
+    font-size: 24px;
+    font-weight: 500;
+  }
+  .skill-set {
+    display: flex;
+    margin-top: 4px;
+    font-size: 16px;
+    .label {
+      &::after {
+        content: ":";
+        margin: 0 4px;
+      }
+    }
+  }
+`;
+const skillList = ["HTML", "CSS", "Java Script", "React", "Next"];
+export default Profile;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import Profile from "@/container/home/Profile";
+import Content from "@/container/layouts/Content";
 import React from "react";
 
-const Homepage = () => {
-  return <main></main>;
+const Homepage = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Content>
+      <h1 className="visually-hidden">이윤슬 블로그 메인 페이지</h1>
+      <Profile />
+      {/* 소개 섹션 */}
+      {/* 주요 게시물 */}
+      {/* 모든 게시물  */}
+    </Content>
+  );
 };
 
 export default Homepage;
