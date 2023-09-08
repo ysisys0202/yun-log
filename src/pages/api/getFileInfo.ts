7076788408b31cf786838afce18626817777cf6b
이윤슬 <ysisys0202@gmail.com>
2023-09-08 21:14:50 +0900

    ✨ 카테고리 정보 가져오는 api 추개

---
+++ src/pages/api/getFileInfo.ts
@@ -0,0 +1,23 @@
+import { getCategories, getPostFiles } from "../../../lib/posts-util";
+
+export default async function categoriesInfo(req: any, res: any) {
+  try {
+    if (req.method === "GET") {
+      const categories = getCategories();
+      const categoriesInfo = categories.map((category) => {
+        const postFiles = getPostFiles(category);
+        return {
+          name: category,
+          fileLength: postFiles.length,
+        };
+      });
+      res.status(200).json(categoriesInfo);
+    } else {
+      // 요청 메소드가 GET이 아닐 경우 처리
+      res.status(405).end(); // Method Not Allowed
+    }
+  } catch (error) {
+    console.error("Error in categoriesInfo API:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
