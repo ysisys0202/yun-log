a95e82989c4c8a71cbf2516e3f4a2bfdac4afdb1
이윤슬 <ysisys0202@gmail.com>
2023-09-17 19:47:32 +0900

    🌱 반응형 관련 상수 추가

---
+++ src/constants/breakPoints.ts
@@ -0,0 +1,10 @@
+export const breakPoints = {
+  lg: 1320,
+  md: 1080,
+  sm: 600,
+};
+export const media = {
+  lg: `screen and (min-width: ${breakPoints.lg}px)`,
+  md: `screen and (min-width: ${breakPoints.md}px)`,
+  sm: `screen and (min-width: ${breakPoints.sm}px)`,
+};
