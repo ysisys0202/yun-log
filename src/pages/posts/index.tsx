7146db5bb0b0e2b6d11b12d8720f53ddb3d9b3e5
이윤슬 <ysisys0202@gmail.com>
2023-09-21 00:59:25 +0900

    ♻️ 전체 포스트 리스트 화면 수정

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,19 +1,20 @@
 import PostList from "@/components/posts/PostList";
-import React from "react";
-import { getAllPosts } from "../../../lib/posts-util";
-import SectionTitle from "@/components/home/SectionTitle";
+import React, { useEffect, useState } from "react";
+import { getPosts } from "../../../lib/posts-util";
+import PostListContainer from "@/container/posts/List";
+import { PostCardType } from "@/components/posts/PostCard";
+import { GetStaticPropsContext } from "next";
 
-const AllPosts = ({ allPostList }: { allPostList: any }) => {
-  return (
-    <>
-      <SectionTitle className="p-4">전체 게시물</SectionTitle>
-      <PostList postList={allPostList} />
-    </>
-  );
+const AllPosts = ({ allPostList }: { allPostList: PostCardType[] }) => {
+  const [mount, setMount] = useState(false);
+  useEffect(() => {
+    setMount(true);
+  }, []);
+  return mount && <PostListContainer postList={allPostList} />;
 };
 
 export function getStaticProps() {
-  const allPostList = getAllPosts();
+  const allPostList = getPosts();
   return {
     props: {
       allPostList,
