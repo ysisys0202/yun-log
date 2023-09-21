91d1ddfd1bf2335614bb6bb7bc942c493d006248
이윤슬 <ysisys0202@gmail.com>
2023-09-22 01:27:40 +0900

    🏷️ PostDetailType 추가

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,3 +1,15 @@
+export type PostDetailType = {
+  title: string;
+  subTitle: string;
+  excerpt: string;
+  thumbNailImage: string;
+  isFeatured: boolean;
+  date: string;
+  content: string;
+  category: string;
+  slug: string;
+};
+
 export type CategoriesInfo = {
   name: string;
   fileLength: number;
