d0f2761ad651c9990973b106061231c699c87900
이윤슬 <ysisys0202@gmail.com>
2023-09-22 01:29:36 +0900

    💄 post detail 페이지 수정

--- src/components/posts/PostContent.tsx
+++ src/components/posts/PostContent.tsx
@@ -1,12 +1,12 @@
-import React from "react";
 import PostHeader from "./PostHeader";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { css } from "@emotion/react";
 import { gray } from "@/constants/colors";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
-const PostContent = ({ post }: { post: any }) => {
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PostDetailType } from "@/types/post";
+const PostContent = ({ post }: { post: PostDetailType }) => {
   const customComponents = {
     p(paragraph: any) {
       const { node } = paragraph;
@@ -25,11 +25,18 @@ const PostContent = ({ post }: { post: any }) => {
       return <p>{paragraph.children}</p>;
     },
     code(code: any) {
-      const { language, children } = code;
+      const { className, children } = code;
+      const language = /language-(\w+)/.exec(className || "");
       return (
-        <SyntaxHighlighter language={language} style={vscDarkPlus}>
-          {children}
-        </SyntaxHighlighter>
+        language && (
+          <SyntaxHighlighter
+            language={language[1]}
+            PreTag="div"
+            style={materialDark}
+          >
+            {String(children).replace(/\n$/, "")}
+          </SyntaxHighlighter>
+        )
       );
     },
   };
@@ -38,20 +45,30 @@ const PostContent = ({ post }: { post: any }) => {
       <PostHeader
         title={post.title}
         date={post.date}
-        headerImage={post.thumbnail}
+        headerImage={post.thumbNailImage}
+        subTitle={post.subTitle}
+        category={post.category}
       />
-      <ReactMarkdown components={customComponents}>
+      <ReactMarkdown
+        components={customComponents}
+        className="post-markdown mt-10"
+      >
         {post.content}
       </ReactMarkdown>
     </article>
   );
 };
 const S = css`
-  margin: 98px 32px;
+  margin: 32px;
   padding: 24px;
   width: calc(100% - 64px);
   max-width: 860px;
   border-radius: 4px;
   background-color: ${gray.background}80;
+  .post-markdown {
+    img {
+      max-width: 400px;
+    }
+  }
 `;
 export default PostContent;
--- src/components/posts/PostHeader.tsx
+++ src/components/posts/PostHeader.tsx
@@ -1,23 +1,41 @@
 import { css } from "@emotion/react";
-import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   title: string;
+  subTitle?: string;
   date: string;
   headerImage?: string;
+  category: string;
 };
-const PostHeader = ({ title, date, headerImage }: Props) => {
+
+const PostHeader = ({
+  title,
+  subTitle,
+  date,
+  headerImage,
+  category,
+}: Props) => {
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const formattedDate = new Date(date).toLocaleDateString("ko-KR", dateOptions);
+
   return (
     <header css={S}>
       {headerImage && (
         <Image src={headerImage} alt={title} width={1200} height={500} />
       )}
       <div className="text-area">
+        <Link href={`/posts/${category}`}>{category}</Link>
         <h1>{title}</h1>
+        {subTitle && <h2>{subTitle}</h2>}
         <div className="definition-item">
           <span className="label">최종 수정일</span>
-          <span className="value">2023년 2월 2일</span>
+          <span className="value">{formattedDate}</span>
         </div>
       </div>
     </header>
@@ -25,19 +43,26 @@ const PostHeader = ({ title, date, headerImage }: Props) => {
 };
 const S = css`
   position: relative;
+  padding: 8px 16px;
   height: 160px;
-  .text-area {
-    display: flex;
-    justify-content: space-between;
-    align-items: end;
+  img {
     position: absolute;
     top: 0;
     left: 0;
-    z-index: 10;
     width: 100%;
+    height: 100%;
+    object-fit: cover;
+    opacity: 0.8;
+  }
+  .text-area {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    z-index: 10;
     height: 100%;
   }
   h1 {
+    margin-top: auto;
     font-size: 36px;
     line-height: 1.4;
   }
