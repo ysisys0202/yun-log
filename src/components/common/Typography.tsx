bd8f97cd47f84bc3f7e72845c8ed95afc45dcb55
이윤슬 <ysisys0202@gmail.com>
2023-09-22 23:36:10 +0900

    💄 Typography 컴포넌트 추가

---
+++ src/components/common/Typography.tsx
@@ -0,0 +1,90 @@
+import { media } from "@/constants/breakPoints";
+import { gray } from "@/constants/colors";
+import {
+  TypographyElementType,
+  TypographyVariantType,
+} from "@/types/typography";
+import { css } from "@emotion/react";
+
+type Props = {
+  variant: TypographyVariantType;
+  element: TypographyElementType;
+  color: string;
+  className?: string;
+  children: React.ReactNode;
+};
+const Typography = ({
+  variant,
+  element,
+  color = gray.primary,
+  className = "",
+  children,
+}: Props) => {
+  const Component = element;
+  return (
+    <Component
+      className={`typography-${variant} ${className}`}
+      style={{ color }}
+      css={S}
+    >
+      {children}
+    </Component>
+  );
+};
+const S = css`
+  line-height: 1.4;
+  letter-spacing: 0.00938em;
+  &[class^="typography-h"] {
+    font-weight: 700;
+  }
+  &[class^="typography-body"] {
+    font-weight: 400;
+  }
+  &.typography-h1 {
+    font-size: 32px;
+  }
+  &.typography-h2 {
+    font-size: 28px;
+  }
+  &.typography-h3 {
+    font-size: 22px;
+  }
+  &.typography-h4 {
+    font-size: 20px;
+  }
+  &.typography-body1 {
+    font-size: 16px;
+  }
+  &.typography-body2 {
+    font-size: 14px;
+  }
+  &.typography-subtitle1 {
+    font-size: 18px;
+    font-weight: 700;
+  }
+  @media ${media.md} {
+    &.typography-h1 {
+      font-size: 36px;
+    }
+    &.typography-h2 {
+      font-size: 32px;
+    }
+    &.typography-h3 {
+      font-size: 28px;
+    }
+    &.typography-h4 {
+      font-size: 24px;
+    }
+    &.typography-body1 {
+      font-size: 16px;
+      font-weight: 700;
+    }
+    &.typography-body2 {
+      font-size: 15px;
+    }
+    &.typography-subtitle1 {
+      font-size: 16px;
+    }
+  }
+`;
+export default Typography;
---
+++ src/types/typography.ts
@@ -0,0 +1,18 @@
+export type TypographyElementType =
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "p"
+  | "span"
+  | "strong";
+export type TypographyVariantType =
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "body1"
+  | "body2"
+  | "caption1";
