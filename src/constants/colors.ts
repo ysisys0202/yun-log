cca4b7cfb1ac4338f0c2a7b6e65c1a8b309898fc
이윤슬 <ysisys0202@gmail.com>
2023-10-02 01:57:52 +0900

    💄 colors  green 상수 추가

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,8 +1,13 @@
 export const colors = {
   black: "#000",
   white: "#fff",
+  blue: "#008E9B",
 };
-
+export const green = {
+  primary: "#5B8982",
+  border: "#CDE2CA",
+  background: "#f4fbf3",
+};
 export const gray = {
   primary: " #333333",
   secondary: "#727272",
