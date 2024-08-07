2c55b68d49d0b7772160e82ae2c497ea0b136f58
이윤슬 <ysisys0202@gmail.com>
2024-08-07 09:26:07 +0900

    🚚  useColorMode 파일 확장자 변경

--- src/hooks/useColorMode.ts
+++ src/hooks/useColorMode.ts
@@ -1,7 +1,5 @@
 import { modeColor } from "@/constants/colors";
 import { colorModeState, value as colorModeValue } from "@/store/colorMode";
-import { ColorThemeType } from "@/types/colorTheme";
-import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 
 export default function useColorMode() {
