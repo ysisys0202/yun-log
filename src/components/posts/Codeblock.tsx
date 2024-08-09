4ff90fde257ea5dcacf1bd45ef93862076f7cce4
이윤슬 <ysisys0202@gmail.com>
2024-08-09 09:45:22 +0900

    💄 Codeblock 컴포넌트 언어타입 표기 추가

--- src/components/posts/Codeblock.tsx
+++ src/components/posts/Codeblock.tsx
@@ -1,19 +1,43 @@
-import React from "react";
+import { css } from "@emotion/react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import CaptionText from "@/components/posts/CaptionText";
+import { gray } from "@/constants/colors";
+import Typography from "@/components/common/Typography";
+
 type Props = {
   children: string | string[];
   language: string;
 };
+
 const Codeblock = ({ children, language }: Props) => {
   return (
-    <div>
-      <SyntaxHighlighter language={language} PreTag="pre" style={materialDark}>
+    <section>
+      <header css={S.codeBlockHeader}>
+        <Typography variant="body1" element="span">
+          {language}
+        </Typography>
+      </header>
+      <SyntaxHighlighter
+        language={language}
+        PreTag="pre"
+        style={materialDark}
+        css={S.codeBlockBody}
+      >
         {children}
       </SyntaxHighlighter>
-    </div>
+    </section>
   );
+};
+
+const S = {
+  codeBlockHeader: css`
+    padding: 8px;
+    border-radius: 4px 4px 0 0;
+    background-color: ${gray.$50};
+  `,
+  codeBlockBody: css`
+    margin: 0 !important;
+  `,
 };
 
 export default Codeblock;
