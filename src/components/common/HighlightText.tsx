83b2697e9c545aa5cd7c4976553077b12500d7f3
이윤슬 <ysisys0202@gmail.com>
2024-08-13 22:55:06 +0900

    💄  HighlightText 컴포넌트 스타일 수정

--- src/components/common/HighlightText.tsx
+++ src/components/common/HighlightText.tsx
@@ -1,26 +1,51 @@
-import { TypographyWeight } from "@/types/typography";
 import { css } from "@emotion/react";
+import { TypographyWeight } from "@/types/typography";
 
 type Props = {
-  fontWeight: TypographyWeight;
-  textColor: string;
-  backgroundColor: string;
+  fontWeight?: TypographyWeight;
+  textColor?: string;
+  backgroundColor?: string;
+  highlightType?: "underline" | "full";
+  decoration?: "underline" | "line-through";
   children: React.ReactNode;
 };
 
 const HighlightText = ({
-  fontWeight,
+  fontWeight = 400,
   textColor,
   backgroundColor,
   children,
+  highlightType = "full",
+  decoration,
 }: Props) => {
-  const style = css`
-    background-color: ${backgroundColor};
-    color: ${textColor};
-    font-weight: ${fontWeight};
+  const styles = [
+    css`
+      color: ${textColor};
+      font-weight: ${fontWeight};
+    `,
+  ];
+  const highlightStyle = css`
+    position: relative;
+    &::before {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      z-index: 1;
+      width: 100%;
+      height: ${highlightType === "full" ? "80%" : "20%"};
+      transform: skewX(-10deg);
+      background-color: ${backgroundColor};
+      opacity: 0.4;
+    }
   `;
+  const decorationStyle = css`
+    text-decoration: ${decoration};
+  `;
+  backgroundColor && styles.push(highlightStyle);
+  decoration && styles.push(decorationStyle);
 
-  return <span css={style}>{children}</span>;
+  return <span css={styles}>{children}</span>;
 };
 
 export default HighlightText;
