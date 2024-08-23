f5159418211af368f3e4fc920b166fc69da7076a
이윤슬 <ysisys0202@gmail.com>
2024-08-23 14:10:02 +0900

    💄 PostImage tailwind 제거

--- src/components/posts/PostImage.tsx
+++ src/components/posts/PostImage.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import { css } from "@emotion/react";
 import CaptionText from "@/components/posts/CaptionText";
+
 type Props = {
   src: string;
   alt: string;
@@ -7,13 +9,19 @@ type Props = {
   height: number;
   caption?: string;
 };
+
 const PostImage = ({ src, alt, width, height, caption }: Props) => {
   return (
     <div>
       <Image {...{ src, alt, width, height }} />
-      {!!caption && <CaptionText className="mt-1">{caption}</CaptionText>}
+      {!!caption && <CaptionText css={S.caption}>{caption}</CaptionText>}
     </div>
   );
 };
 
+const S = {
+  caption: css`
+    margin-top: 4px;
+  `,
+};
 export default PostImage;
