b3676f5f5ee5e6eee31817034b7ee7762be6392f
이윤슬 <ysisys0202@gmail.com>
2024-08-26 02:33:28 +0900

    💄  SideMenu UI 수정

---
+++ src/components/sideMenu/SideNav.tsx
@@ -0,0 +1,71 @@
+import Link from "next/link";
+import { css } from "@emotion/react";
+import useCategoriesInfo from "@/hooks/useNavCategories";
+import Typography from "@/components/common/Typography";
+import { useRouter } from "next/router";
+import { colorVars } from "@/constants/cssVariables";
+
+const SideNav = () => {
+  const router = useRouter();
+  const { pathname, query } = router;
+  const currentCategory = pathname === "/posts" ? "전체" : query.category;
+  const { navCategories } = useCategoriesInfo();
+  return (
+    <nav css={S.self}>
+      <Typography variant="h4" element="h2">
+        게시글 목록
+      </Typography>
+      <ul css={S.navList}>
+        {navCategories.map(({ name, fileLength, link }) => {
+          const isActive = currentCategory === name;
+          return (
+            <li css={S.navItem} className={`${isActive ? "is-active" : ""}`}>
+              <Link href={link}>
+                <Typography variant="subtitle1" element="span">
+                  {name} ({fileLength})
+                </Typography>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+const S = {
+  self: css`
+    margin-top: 80px;
+  `,
+  navList: css`
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    margin-top: 12px;
+  `,
+
+  navItem: css`
+    span {
+      position: relative;
+      padding-right: 24px;
+      &::after {
+        content: "";
+        position: absolute;
+        top: 2px;
+        right: 0;
+        width: 12px;
+        height: 12px;
+        background-color: transparent;
+      }
+    }
+    &.is-active,
+    &:hover {
+      span {
+        &::after {
+          background-color: ${colorVars.greenPrimary};
+        }
+      }
+    }
+  `,
+};
+export default SideNav;
--- src/container/layouts/SideMenu.tsx
+++ src/container/layouts/SideMenu.tsx
@@ -1,119 +1,46 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import { css } from "@emotion/react";
-import { useRecoilState, useRecoilValue } from "recoil";
 import Link from "next/link";
-import { CategoriesInfo } from "@/types/post";
-import { colors } from "@/constants/colors";
 import { media } from "@/constants/breakPoints";
-import { mobileMenuState } from "@/store/mobileMenu";
-import useMediaQuery from "@/hooks/useMediaQuery";
 import Logo from "@/components/common/Logo";
-import MenuButton from "@/components/common/MenuButton";
-import Tag from "@/components/common/Tag";
-import TagList from "@/components/common/TagList";
 import { colorVars } from "@/constants/cssVariables";
-import useNavCategories from "@/hooks/useNavCategories";
+import SideNav from "@/components/sideMenu/SideNav";
 
 const SideMenu = () => {
-  const router = useRouter();
-  const query = router.query;
-  const currentCategory = query.category as string;
-  const [mount, setMount] = useState(false);
-  const { navCategories } = useNavCategories();
-  const [mobileMenuActive, setMobileMenuActive] =
-    useRecoilState(mobileMenuState);
-  const isMediaMd = useMediaQuery(media.md);
-  const mobileMenuButtonClickHandler = () => {
-    setMobileMenuActive((prevState: boolean) => !prevState);
-  };
-  useEffect(() => {
-    setMount(true);
-  }, []);
-
   return (
-    mount && (
-      <aside
-        className={`side-menu ${mobileMenuActive ? "is-active" : ""}`}
-        css={S}
-        style={{ borderRight: `1px solid ${colorVars.border}` }}
-      >
-        <header>
-          <Link href="/" className="logo">
-            <Logo />
-            <strong className="visually-hidden">이윤슬 개발 블로그</strong>
-          </Link>
-          {!isMediaMd && (
-            <MenuButton
-              isActive={mobileMenuActive}
-              onClick={mobileMenuButtonClickHandler}
-              className="mobile-menu-button"
-            />
-          )}
-        </header>
-        <TagList className="tab-list">
-          {navCategories.map((category: CategoriesInfo) => (
-            <Link href={`/posts/${category.name}`} key={category.name}>
-              <Tag
-                variant="outlined"
-                backgroundColor={
-                  currentCategory === category.name
-                    ? colorVars.greenPrimary
-                    : "transparent"
-                }
-                borderColor={colorVars.greenBorder}
-                textColor={
-                  currentCategory === category.name
-                    ? colorVars.greenBackground
-                    : colorVars.greenPrimary
-                }
-              >
-                {category.name} {`(${category.fileLength})`}
-              </Tag>
-            </Link>
-          ))}
-        </TagList>
-      </aside>
-    )
+    <aside
+      css={S.self}
+      style={{ borderRight: `1px solid ${colorVars.border}` }}
+    >
+      <Link href="/" className="logo" css={S.logo}>
+        <Logo />
+        <strong className="visually-hidden">YUN 개발 블로그</strong>
+      </Link>
+      <SideNav />
+    </aside>
   );
 };
-const S = css`
-  display: flex;
-  flex-direction: column;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 100;
-  padding: 16px;
-  width: 80%;
-  height: 100dvh;
-  background-color: ${colorVars.greenBackground};
-  transform: translateX(100vw);
-  transition: transform 200ms ease-in-out;
-  &.is-active {
-    transform: translateX(calc(100vw - 100%));
-  }
-  .tab-list {
-    margin-top: auto;
-    margin-bottom: 120px;
-  }
-  .mobile-menu-button {
-    position: absolute;
-    top: 10px;
-    right: 20px;
-  }
-  footer {
-    margin: 160px 0 40px;
-    text-align: center;
-  }
-  .scroll-text {
-    font-size: 16px;
-    color: ${colors.white};
-  }
-  @media ${media.md} {
-    width: 20%;
-    min-width: 220px;
-    transform: translateX(0);
-  }
-`;
+const S = {
+  self: css`
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 100;
+    padding: 16px 32px;
+    width: 80%;
+    height: 100dvh;
+    background-color: ${colorVars.greenBackground};
+    transform: translateX(100vw);
+    transition: transform 200ms ease-in-out;
+    @media ${media.md} {
+      width: 20%;
+      min-width: 220px;
+      transform: translateX(0);
+    }
+  `,
+  logo: css`
+    margin-left: -4px;
+  `,
+};
 export default SideMenu;
