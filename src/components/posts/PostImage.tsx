f9f6f2b4a50b9d081ab9b4cabda268402e973af8
이윤슬 <ysisys0202@gmail.com>
2024-09-01 01:11:06 +0900

    💄 PostImage 높이 수정

--- src/components/posts/PostImage.tsx
+++ src/components/posts/PostImage.tsx
@@ -13,10 +13,15 @@ type Props = {
 const PostImage = ({ src, alt, width, height, caption }: Props) => {
   return (
     <div>
-      <Image {...{ src, alt, width, height }} />
+      <Image {...{ src, alt, width, height }} css={S.image} />
       {!!caption && <CaptionText>{caption}</CaptionText>}
     </div>
   );
 };
-
+const S = {
+  image: css`
+    max-width: 100%;
+    height: auto;
+  `,
+};
 export default PostImage;
