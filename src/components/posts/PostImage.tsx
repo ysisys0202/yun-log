03de1fcf8c080353862169135723250dbbde40cf
이윤슬 <ysisys0202@gmail.com>
2024-09-16 07:10:30 +0900

    💄 PostImage 외곽선 추가

--- src/components/posts/PostImage.tsx
+++ src/components/posts/PostImage.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { css } from "@emotion/react";
 import CaptionText from "@/components/posts/CaptionText";
+import { colorVars } from "@/constants/cssVariables";
 
 type Props = {
   src: string;
@@ -22,6 +23,7 @@ const S = {
   image: css`
     max-width: 100%;
     height: auto;
+    border: 1px solid ${colorVars.border};
   `,
 };
 export default PostImage;
