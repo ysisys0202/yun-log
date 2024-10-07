f5ddaa2e0b82e5677a03c851312b2c92562a03b7
이윤슬 <ysisys0202@gmail.com>
2024-10-07 23:04:40 +0900

    💄 Tag 컴포넌트 forwardRef 추가 및 변수명 변경

--- src/components/common/Tag.tsx
+++ src/components/common/Tag.tsx
@@ -1,6 +1,5 @@
-import React, { HTMLProps } from "react";
+import React, { HTMLProps, forwardRef } from "react";
 import { SerializedStyles, css } from "@emotion/react";
-import { colors } from "@/constants/colors";
 import { colorVars } from "@/constants/cssVariables";
 
 export type TagProps = {
@@ -14,68 +13,69 @@ export type TagProps = {
   propsCss?: SerializedStyles;
 } & Omit<HTMLProps<HTMLDivElement>, "size">;
 
-const Tag = ({
-  className = "",
-  backgroundColor = "transparent",
-  textColor = colorVars.primary,
-  borderColor = colorVars.border,
-  size = "md",
-  children,
-  variant = "contained",
-  propsCss,
-  ...rest
-}: TagProps) => {
-  const dynamicStyles = [
-    css`
-      background-color: ${backgroundColor};
-      color: ${textColor};
-      border-color: ${borderColor};
-    `,
-    propsCss,
-  ];
-  const styles = [
-    S.defalut,
-    S.size[size],
-    S.variant[variant],
-    ...dynamicStyles,
-  ];
+const Tag = forwardRef<HTMLDivElement, TagProps>(
+  (
+    {
+      className = "",
+      backgroundColor = "transparent",
+      textColor = colorVars.primary,
+      borderColor = colorVars.border,
+      size = "md",
+      children,
+      variant = "contained",
+      propsCss,
+      ...props
+    },
+    ref
+  ) => {
+    const styles = [
+      tagStyle.base,
+      tagStyle[size],
+      tagStyle[variant],
+      css`
+        background-color: ${backgroundColor};
+        color: ${textColor};
+        border-color: ${borderColor};
+      `,
+    ];
+    propsCss && styles.push(propsCss);
 
-  return (
-    <div css={styles} {...rest}>
-      {children}
-    </div>
-  );
-};
+    return (
+      <div ref={ref} css={styles} {...props}>
+        {children}
+      </div>
+    );
+  }
+);
 
-const S = {
-  defalut: css`
+const tagStyle = {
+  base: css`
     display: inline-flex;
     justify-content: center;
     align-items: center;
     border-radius: 20px;
   `,
-  size: {
-    sm: css`
-      height: 26px;
-      padding: 0 8px;
-      font-size: 12px;
-      line-height: 1;
-    `,
-    md: css`
-      height: 30px;
-      padding: 0 12px;
-      font-size: 14px;
-      line-height: 1.4;
-    `,
-  },
-  variant: {
-    contained: css`
-      border: none;
-    `,
-    outlined: css`
-      border: 1px solid;
-    `,
-  },
+  // NOTE: size styles
+  sm: css`
+    height: 26px;
+    padding: 0 8px;
+    font-size: 12px;
+    line-height: 1;
+  `,
+  md: css`
+    height: 30px;
+    padding: 0 12px;
+    font-size: 14px;
+    line-height: 1.4;
+  `,
+  // NOTE: variant styles
+  contained: css`
+    border: none;
+  `,
+  outlined: css`
+    border: 1px solid;
+  `,
 };
 
+Tag.displayName = "Tag";
 export default Tag;
