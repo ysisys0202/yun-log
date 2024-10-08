ccaf55db1f76f8ebf2efd01d247e50c8c5602c84
이윤슬 <ysisys0202@gmail.com>
2024-10-08 20:40:45 +0900

    ✨ 초 변환 함수 추가

---
+++ src/utils/convertSeconds.ts
@@ -0,0 +1,13 @@
+type ConvertSecondsParams = {
+  hours?: number;
+  minutes?: number;
+  seconds?: number;
+};
+
+const convertSeconds = ({
+  hours = 0,
+  minutes = 0,
+  seconds = 0,
+}: ConvertSecondsParams): number => {
+  return hours * 3600 + minutes * 60 + seconds;
+};
