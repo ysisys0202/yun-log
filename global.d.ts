c3bc0dc858ecdc9d8886ddff1a9d98f72f3e34e9
이윤슬 <ysisys0202@gmail.com>
2024-12-17 21:41:13 +0900

    🏷️ global.d.ts svg content type 변경

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,5 @@
 declare module "*.svg" {
-  import React = require("react");
+  const content: any;
+  export default content;
   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
 }
