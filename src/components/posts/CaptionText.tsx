cfacbecb70d0d99da79bf2f8fb09ffdbc08718ae
이윤슬 <ysisys0202@gmail.com>
2024-12-30 21:49:43 +0900

    💄 PostImage Caption 줄바꿈 추가

--- src/components/posts/CaptionText.tsx
+++ src/components/posts/CaptionText.tsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Typography from "../common/Typography";
 import { colorVars } from "@/constants/cssVariables";
+import { css } from "@emotion/react";
 
 type Props = {
   children: React.ReactNode;
   className?: string;
 };
+
+const lineBreak = (text: ReactNode) => {
+  const splitedText = text?.toString().split("\\n");
+
+  return splitedText?.map((line, index) => (
+    <>
+      {line.trim()}
+      {index < splitedText.length - 1 && <br />}
+    </>
+  ));
+};
+
 const CaptionText = ({ children, className }: Props) => {
   return (
     <Typography
       variant="body2"
       as="p"
       color={colorVars.tertiary}
+      css={style}
       {...(className && { className: className })}
     >
-      {children}
+      {lineBreak(children)}
     </Typography>
   );
 };
 
+const style = css`
+  width: 100%;
+  word-break: break-all;
+`;
 export default CaptionText;
