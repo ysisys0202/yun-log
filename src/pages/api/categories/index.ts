e60c1cbe4c45f60cc3952d06475d5e2cffb7e6f5
이윤슬 <ysisys0202@gmail.com>
2024-12-31 21:37:26 +0900

    ♻️ /categories api 불필요한 코드 제거

--- src/pages/api/categories/index.ts
+++ src/pages/api/categories/index.ts
@@ -1,19 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getCategories } from "libs/post";
-import convertSeconds from "@/utils/convertSeconds";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (req.method === "GET") {
       const categories = await getCategories();
-      res.setHeader(
-        "Cache-Control",
-        `public, max-age=${convertSeconds({ minutes: 30 })}`
-      );
       res.status(200).json(categories);
-    } else {
-      // NOTE: 요청 메소드가 GET이 아닐 경우 처리
-      res.status(405).end();
     }
   } catch (error) {
     console.error("카테고리 목록을 불러오는 데 실패했습니다.", error);
