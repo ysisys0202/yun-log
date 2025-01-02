3ddb6aa0656f329b2c3be076f8054ebaa5cb70a2
이윤슬 <ysisys0202@gmail.com>
2025-01-02 10:38:30 +0900

    🌱 API_BASE_URL 값 변경

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,5 +1,2 @@
-import { isClient } from "@/utils/common";
-
-export const API_BASE_URL = isClient
-  ? ""
-  : process.env.NEXT_PUBLIC_API_BASE_URL;
+export const API_BASE_URL =
+  process.env.VERCEL_URL || process.env.NEXT_PUBLIC_API_BASE_URL || "";
