45d64824fe20ad8a2f2a6158cdfde32adf4b84d3
이윤슬 <ysisys0202@gmail.com>
2025-01-02 18:50:45 +0900

    ♻️ fetcher 함수 추가 및 적용

--- src/services/post.ts
+++ src/services/post.ts
@@ -1,8 +1,7 @@
-import { API_BASE_URL } from "@/constants/path";
-import { Category, PostData } from "@/types/post";
-import createQueryString from "@/utils/createQueryString";
-import { handleError } from "@/utils/error";
 import { GetPostsParams, getPostDataParams } from "libs/post";
+import { Category, PostData } from "@/types/post";
+import fetcher from "@/utils/fetcher";
+import createQueryString from "@/utils/createQueryString";
 
 export const fetchPosts = async ({
   categoryName,
@@ -11,24 +10,14 @@ export const fetchPosts = async ({
   filter,
   size,
 }: GetPostsParams): Promise<PostData[] | undefined> => {
-  try {
-    const queryString = createQueryString({
-      categoryId,
-      categoryName,
-      sort,
-      filter,
-      size,
-    });
-    const response = await fetch(`${API_BASE_URL}/api/posts${queryString}`);
-    if (!response.ok) {
-      handleError("포스트 목록을 불러오는 데 실패했습니다.");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-    handleError("service 포스트 목록을 불러오는 데 실패했습니다.");
-  }
+  const queryString = createQueryString({
+    categoryId,
+    categoryName,
+    sort,
+    filter,
+    size,
+  });
+  return fetcher(`/posts${queryString}`);
 };
 
 export const fetchPost = async ({
@@ -36,36 +25,16 @@ export const fetchPost = async ({
   categoryId,
   postId,
 }: getPostDataParams): Promise<PostData | undefined> => {
-  try {
-    const queryString = createQueryString({
-      categoryId,
-      categoryName,
-      postId,
-    });
-    const response = await fetch(
-      `${API_BASE_URL}/api/posts/detail${queryString}`
-    );
-    if (!response.ok) {
-      handleError("포스트를 불러오는 데 실패했습니다.");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    handleError("포스트를 불러오는 데 실패했습니다.");
-  }
+  const queryString = createQueryString({
+    categoryId,
+    categoryName,
+    postId,
+  });
+  return fetcher(`/posts/detail${queryString}`);
 };
 
 export const fetchPostCategories = async (): Promise<
   Category[] | undefined
 > => {
-  try {
-    const response = await fetch(`${API_BASE_URL}/api/categories`);
-    if (!response.ok) {
-      handleError("카테고리 정보를 불러오는 데 실패했습니다.");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    handleError("카테고리 정보를 불러오는 데 실패했습니다.");
-  }
+  return fetcher("/categories");
 };
