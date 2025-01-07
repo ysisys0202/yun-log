0ff4204694e829830a007af1e976ff88ed2ef019
이윤슬 <ysisys0202@gmail.com>
2025-01-07 15:57:43 +0900

    🔧 cypress.config.js baseUrl env 추가

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,15 @@ const { defineConfig } = require("cypress");
 const dotenv = require("dotenv");
 
 dotenv.config();
-
+const baseUrl = "http://localhost:3000";
 module.exports = defineConfig({
   e2e: {
     projectId: process.env.NEXT_PUBLIC_CYPRESS_ID,
-    baseUrl: "http://localhost:3000",
+    baseUrl,
     setupNodeEvents(on, config) {},
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+  },
+  env: {
+    baseUrl,
   },
 });
